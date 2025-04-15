ad3744a86db48d7d4080e520e5901fc3cfa9fe29
micoforreal <mjjames006@gmail.com>
2025-04-15 17:50:30 +0100

    updated

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,7 +5,7 @@ import styles from "../Styles/Global.css";
 import { TfiDirectionAlt } from "react-icons/tfi";
 import { AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 import { AiOutlineMail } from "react-icons/ai";
-import { BsTwitter } from "react-icons/bs";
+import { BsTelegram, BsTwitter } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
 import { motion } from "framer-motion";
 
@@ -40,7 +40,7 @@ export const Contact = () => {
         margin={"auto"}
       >
         <a
-          href="https://www.instagram.com/micoforreal/"
+          href="https://www.instagram.com/justonemylo/"
           target={"_blank"}
         >
           <Box
@@ -88,13 +88,13 @@ export const Contact = () => {
               flexDirection={"column"}
             >
               <h2>Instagram</h2>
-              <p style={{ color: "#999999" }}>micoforreal</p>
+              <p style={{ color: "#999999" }}>@justonemylo</p>
             </Box>
           </Box>
         </a>
 
        
-        <a href="https://x.com/micoforreal" target={"_blank"}>
+        <a href="https://x.com/justonemylo" target={"_blank"}>
           <Box
             w={"220px"}
             as={motion.div}
@@ -138,7 +138,7 @@ export const Contact = () => {
               flexDirection={"column"}
             >
               <h2>Twitter</h2>
-              <p style={{ color: "#999999" }}>micoforreal</p>
+              <p style={{ color: "#999999" }}>@justonemylo</p>
             </Box>
           </Box>
         </a>
@@ -188,6 +188,59 @@ export const Contact = () => {
             >
               <h2>Email</h2>
               <p style={{ color: "#999999" }}>mjjames006@gmail.com
+              </p>
+            </Box>
+          </Box>
+        </a>
+
+
+
+
+        <a href="https://t.me/justonemylo" target={"_blank"}>
+          <Box
+            w={"220px"}
+            as={motion.div}
+            initial={variant ? { x: 100 } : { x: 200 }}
+            whileInView={{ x: 0,opacity:1 }}
+            whileHover={{ y: -15 }}
+            transition={{
+              delay: 2,
+              duration: 2,
+              type: "spring",
+              stiffness: 300,
+            }}
+            // viewport={{ once: false, amount: 0.6 }}
+            p="20px"
+            display="flex"
+            flexDirection={"column"}
+            alignItems={"center"}
+            h="180px"
+            justifyContent="space-between"
+            borderRadius={"8px"}
+            boxShadow="rgba(80, 202, 246, 0.84) 0px 2px 6px"
+          >
+            <Box
+              bgColor={"#1a1a1a"}
+              borderRadius="50%"
+              h={"80px"}
+              w="80px"
+              color={"#4dbdfa"}
+              display="flex"
+              alignItems={"center"}
+              justifyContent="center"
+              fontSize={"35px"}
+            >
+              <BsTelegram/>
+            </Box>
+            <Box
+              color={"#fff"}
+              display="flex"
+              alignItems={"center"}
+              justifyContent="center"
+              flexDirection={"column"}
+            >
+              <h2>Telegram</h2>
+              <p style={{ color: "#999999" }}>@justonemylo
               </p>
             </Box>
           </Box>
--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -17,7 +17,7 @@ export const Project = () => {
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: false, amount: 1 }}
             transition={{
-              duration: 0.5,
+              duration: 0.3,
             }}
             className={"sectionHeading"}
             data-outline="Projects"
@@ -45,28 +45,37 @@ export const Project = () => {
                 <h2 className={styles.projectTitle}>MultiXender</h2>
 
                 <p className={styles.projectDesc}>
-                  A platform that let you send token to multiple address in one click 
+                  A platform that let you send token to multiple address in one
+                  click
                   <h3>Features</h3>
-                  <ul style={{ marginLeft: "30px", marginBottom:"10px" }}>
+                  <ul style={{ marginLeft: "30px", marginBottom: "10px" }}>
                     <li>Connect Wallet</li>
                     <li>Fetch user Account balance</li>
                     <li>Sign and Send Trasaction with Smart Contract</li>
-                     </ul>
-                  <h2 style={{marginBottom:"10px"}}><b><i> Tech Stack :</i></b> Tailwind CSS | Reactjs
-                  | React-Hook-Form | Rust </h2>
-                  <h2  style={{marginBottom:"10px"}}>
-                   <b><i> Area of Responsibility :</i></b>  My role in the project involves constructing
-                      responsive UI based on Figma designs, seamlessly integrating APIs
-                      across all pages. I consistently implement platform modifications in
-                      alignment with evolving business requirements.
-
+                  </ul>
+                  <h2 style={{ marginBottom: "10px" }}>
+                    <b>
+                      <i> Tech Stack :</i>
+                    </b>{" "}
+                    Tailwind CSS | Reactjs | React-Hook-Form | Rust{" "}
                   </h2>
-
-
-                  <h2><b><i>Date: </i></b> Dec 10 - Dec 24: 2024</h2>
+                  <h2 style={{ marginBottom: "10px" }}>
+                    <b>
+                      <i> Area of Responsibility :</i>
+                    </b>{" "}
+                    My role in the project involves constructing responsive UI
+                    based on Figma designs, seamlessly integrating APIs across
+                    all pages. I consistently implement platform modifications
+                    in alignment with evolving business requirements.
+                  </h2>
+                  <h2>
+                    <b>
+                      <i>Date: </i>
+                    </b>{" "}
+                    Dec 10 - Dec 24: 2024
+                  </h2>
                 </p>
                 <div className={styles.buttons}>
-                
                   <a
                     href="https://multixenderfullstack.vercel.app/"
                     target="_blank"
@@ -99,29 +108,37 @@ export const Project = () => {
                 <h2 className={styles.projectTitle}>Ex Tap App</h2>
 
                 <p className={styles.projectDesc}>
-                  This is fun crypto tap game that users play to earn the ex Token
-
+                  This is fun crypto tap game that users play to earn the ex
+                  Token
                   <h2>Features</h2>
-                  <ul style={{ marginLeft: "30px", marginBottom:'10px' }}>
+                  <ul style={{ marginLeft: "30px", marginBottom: "10px" }}>
                     <li>Leaderboard </li>
-                 <li>refferal system</li>
-            <li> challenges and reward</li>
-            
-                              </ul>
-                  
-                  <h2 style={{marginBottom:"10px"}}><b><i>Tech Stack :</i></b> React | Tailwind CSS | Expressjs | Mongodb
+                    <li>refferal system</li>
+                    <li> challenges and reward</li>
+                  </ul>
+                  <h2 style={{ marginBottom: "10px" }}>
+                    <b>
+                      <i>Tech Stack :</i>
+                    </b>{" "}
+                    React | Tailwind CSS | Expressjs | Mongodb
                   </h2>
-                  <h2  style={{marginBottom:"10px"}}>
-                   <b><i>Area of Responsibility :</i></b> Building the backend with express and connecting it to the frontend of the app , handling users 
-                   state and automatically authenticating telegram users with the api 
+                  <h2 style={{ marginBottom: "10px" }}>
+                    <b>
+                      <i>Area of Responsibility :</i>
+                    </b>{" "}
+                    Building the backend with express and connecting it to the
+                    frontend of the app , handling users state and automatically
+                    authenticating telegram users with the api
                   </h2>
-                  
-                  <h2><b><i>Date: </i></b> Jan 30 2025 - present </h2>
+                  <h2>
+                    <b>
+                      <i>Date: </i>
+                    </b>{" "}
+                    Jan 30 2025 - present{" "}
+                  </h2>
                 </p>
 
-                
                 <div className={styles.buttons}>
-                  
                   <a
                     href="https://t.me/ex1tonbot/EXPTE"
                     target="_blank"
@@ -134,6 +151,66 @@ export const Project = () => {
             </motion.div>
             {/* ========================================================================================================================================= */}
 
+            <motion.div
+              className={styles.item}
+              initial={{ x: -70, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              viewport={{ once: false, amount: 0.1 }}
+              transition={{
+                duration: 3.5,
+                type: "spring",
+                stiffness: 300,
+              }}
+              // transition={{ delay: 0.1 }}
+            >
+              <div className={styles.left}>
+                <div className={styles.img}>
+                  <img src="genesis_store.png" alt="" />
+                </div>
+              </div>
+              <div className={styles.right}>
+                <h2 className={styles.projectTitle}>Genesis Store</h2>
+
+                <p className={styles.projectDesc}>
+                  This is an ecommerce app that let you buy and sell products
+                  <h2>Features</h2>
+                  <ul style={{ marginLeft: "30px", marginBottom: "10px" }}>
+                    <li>Search</li>
+                    <li>Product Filter</li>
+                    <li>Cart</li>
+                  </ul>
+                  <h2 style={{ marginBottom: "10px" }}>
+                    <b>
+                      <i>Tech Stack :</i>
+                    </b>{" "}
+                    React | Tailwind CSS | Expressjs | Postgresql
+                  </h2>
+                  <h2 style={{ marginBottom: "10px" }}>
+                    <b>
+                      <i>Area of Responsibility :</i>
+                    </b>{" "}
+                    build backend with express and connecting it to the reactjs frontend of the app
+                    , handling users state and automatically authenticating
+                  </h2>
+                  <h2>
+                    <b>
+                      <i>Date: </i>
+                    </b>{" "}
+                    Oct 01 2024 - Nov 3 2024{" "}
+                  </h2>
+                </p>
+
+                <div className={styles.buttons}>
+                  <a
+                    href="https://genesis-store-fe.vercel.app/"
+                    target="_blank"
+                    className={styles.primaryBtn}
+                  >
+                    Live Link{" "}
+                  </a>
+                </div>
+              </div>
+            </motion.div>
           </div>
         </div>
       </section>
