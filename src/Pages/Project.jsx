f82add9748e5bb668a6a17f7cf32eb39f24da6bf
micoforreal <mjjames006@gmail.com>
2025-05-14 12:13:21 +0100

    projects updated

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -6,6 +6,55 @@ import styles from "../Styles/Projects.module.css";
 import { motion } from "framer-motion";
 import jiomartImg from "./Images/jiomart.png";
 
+
+const projectData = [
+  {
+    title: "MultiXender",
+    description: "A platform that let you send token to multiple address in one click",
+    features: [
+      "Connect Wallet",
+      "Fetch user Account balance",
+      "Sign and Send Trasaction with Smart Contract",
+    ],
+    techStack: "Tailwind CSS | Reactjs | React-Hook-Form | Rust",
+    areaOfResponsibility:
+      "My role in the project involves constructing responsive UI based on Figma designs, seamlessly integrating APIs across all pages. I consistently implement platform modifications in alignment with evolving business requirements.",
+    date: "Nov 1 - Dec 24: 2024",
+    image: "multixender.png",
+    liveLink: "https://multixenderfullstack.vercel.app/",
+  },
+  {
+    title: "Ex Tap App",
+    description: "This is fun crypto tap game that users play to earn the ex Token",
+    features: [
+      "Leaderboard",
+      "refferal system",
+      "challenges and reward",
+    ],
+    techStack: "React | Tailwind CSS | Expressjs | Mongodb",
+    areaOfResponsibility:
+      "Building the backend with express and connecting it to the frontend of the app , handling users state and automatically authenticating telegram users with the api",
+    date: "Jan 1 2025 - Feb 1 2025",
+    image: "tapapp.png",
+    liveLink: "https://t.me/ex1tonbot/EXPTE",
+  },
+  {
+    title: "Genesis Store",
+    description: "This is an ecommerce app that let you buy and sell products",
+    features: [
+      "Search",
+      "Product Filter",
+      "Cart",
+    ],
+    techStack: "React | Tailwind CSS | Expressjs | Postgresql",
+    areaOfResponsibility:
+      "build backend with express and connecting it to the reactjs frontend of the app , handling users state and automatically authenticating",
+    date: "Oct 01 2024 - Nov 3 2024",
+    image: "genesis_store.png",
+    liveLink: "https://genesis-store-fe.vercel.app/",
+  },
+];
+
 export const Project = () => {
   const { projectRef } = useContext(RefContext);
   return (
@@ -25,7 +74,11 @@ export const Project = () => {
             Projects
           </motion.h2>
           <div className={styles.containerThree}>
-            <motion.div
+
+            {projectData.map((project, index) => (
+
+              <motion.div
+              key={index}
               className={styles.item}
               initial={{ x: 70, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
@@ -35,59 +88,57 @@ export const Project = () => {
                 type: "spring",
                 stiffness: 300,
               }}
-            >
+              >
+
               <div className={styles.left}>
                 <div className={styles.img}>
-                  <img src="multixender.png" alt="" />
+                  <img src={project.image} alt="" />
                 </div>
               </div>
               <div className={styles.right}>
-                <h2 className={styles.projectTitle}>MultiXender</h2>
+                <h2 className={styles.projectTitle}>{project.title}</h2>
 
                 <p className={styles.projectDesc}>
-                  A platform that let you send token to multiple address in one
-                  click
+                {project.description}
                   <h3>Features</h3>
                   <ul style={{ marginLeft: "30px", marginBottom: "10px" }}>
-                    <li>Connect Wallet</li>
-                    <li>Fetch user Account balance</li>
-                    <li>Sign and Send Trasaction with Smart Contract</li>
+                    { project.features && project.features.map((feature, index) => (
+                      <li key={index}>{feature}</li>
+                    ))} 
                   </ul>
                   <h2 style={{ marginBottom: "10px" }}>
                     <b>
                       <i> Tech Stack :</i>
                     </b>{" "}
-                    Tailwind CSS | Reactjs | React-Hook-Form | Rust{" "}
+                    {project.techStack}{" "}
                   </h2>
                   <h2 style={{ marginBottom: "10px" }}>
                     <b>
                       <i> Area of Responsibility :</i>
                     </b>{" "}
-                    My role in the project involves constructing responsive UI
-                    based on Figma designs, seamlessly integrating APIs across
-                    all pages. I consistently implement platform modifications
-                    in alignment with evolving business requirements.
+                    {project.areaOfResponsibility}
                   </h2>
                   <h2>
                     <b>
                       <i>Date: </i>
                     </b>{" "}
-                    Dec 10 - Dec 24: 2024
+                    {project.date}{" "}
                   </h2>
                 </p>
                 <div className={styles.buttons}>
                   <a
-                    href="https://multixenderfullstack.vercel.app/"
+                    href={project.liveLink}
                     target="_blank"
                     className={styles.primaryBtn}
-                  >
+                    >
                     Live Link
                   </a>
                 </div>
               </div>
             </motion.div>
+                  ))}
             {/* ========================================================================================================================================= */}
-            <motion.div
+            {/* <motion.div
               className={styles.item}
               initial={{ x: -70, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
@@ -148,9 +199,9 @@ export const Project = () => {
                   </a>
                 </div>
               </div>
-            </motion.div>
+            </motion.div> */}
             {/* ========================================================================================================================================= */}
-
+{/* 
             <motion.div
               className={styles.item}
               initial={{ x: -70, opacity: 0 }}
@@ -210,7 +261,7 @@ export const Project = () => {
                   </a>
                 </div>
               </div>
-            </motion.div>
+            </motion.div> */}
           </div>
         </div>
       </section>
